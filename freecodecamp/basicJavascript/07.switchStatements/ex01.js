c45516fc779e8df0301b0473b2d9558ec4454161
sergiotvidal <sergiotvidal2@gmail.com>
2019-04-07 19:46:16 +0200

    some changes

---
+++ freecodecamp/basicJavascript/07.switchStatements/ex01.js
@@ -0,0 +1,20 @@
+// Write a switch statement which tests val and sets answer for the following conditions:
+// 1 - "alpha"
+// 2 - "beta"
+// 3 - "gamma"
+// 4 - "delta"
+
+function caseInSwitch(val) {
+    let answer = "";
+    switch(val){
+        case 1: answer = "alpha"; 
+        break;
+        case 2: answer = "beta"; 
+        break;
+        case 3: answer = "gamma"; 
+        break;
+        case 4: answer = "delta";
+        break;
+    };
+    return answer;
+}
